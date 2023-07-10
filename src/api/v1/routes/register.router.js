6bf542cb64dc598a4e7c75190fbf29d52036963b
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2023-07-09 19:43:21 -0600

    adds

--- src/api/v1/routes/register.router.js
+++ src/api/v1/routes/register.router.js
@@ -2,6 +2,7 @@ import * as express from 'express'
 import registerController from '../controllers/register.controller'
 import { validateCreateAccount,validByCompany } from '../validators/user.validator'
 
+//agregando rutas
 export default express
 .Router()
 .post('/sendAccessCode',registerController.sendAccesCode)
